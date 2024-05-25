5ed612a9b9d77bbc7d98cfa81b0485d3770bf999
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-25 13:20:56 -0300

    Implementado a funcionalidade `Duelar`

--- src/components/personagem-view.js
+++ src/components/personagem-view.js
@@ -1,9 +1,15 @@
+import { Personagem } from "../modules/personagem.js"
+import { mostrarModal } from "./modal.js"
+
 export class PersonagemView {
     personagens
+    personagensSelecionados
 
     constructor(personagens) {
         this.ulPersonagens = document.querySelector('ul#personagens')
         this.personagens = personagens
+        this.personagensSelecionados = []
+        this.escutarEventoDuelo()
     }
 
     render() {
@@ -18,9 +24,9 @@ export class PersonagemView {
         const personagemLI = document.createElement('li')
         personagemLI.classList.add('personagem', personagem.constructor.tipo)
 
-        //const estaSelecionado = this.personagensSelecionados.indexOf(personagem) !== -1 //sintaxe para quando encontra no array
+        const estaSelecionado = this.personagensSelecionados.indexOf(personagem) !== -1 //sintaxe para quando encontra no array
 
-        //if (estaSelecionado) personagemLI.classList.add('selecionado')
+        if (estaSelecionado) personagemLI.classList.add('selecionado')
 
         personagemLI.innerHTML =
 
@@ -44,7 +50,7 @@ export class PersonagemView {
                 <h3 class="nome">${personagem.nome}</h3>
             </div>
             <div class="container-descricao">
-                <p class="descricao"></p>
+                <p class="descricao">${personagem.constructor.descricao}</p>
             </div>
         </div>
         <div class="container-inferior">
@@ -68,46 +74,46 @@ export class PersonagemView {
         }*/
 
 
-        /*personagemLI.onclick = () => {
+        personagemLI.onclick = () => {
             const jaTem2Selecionados = this.personagensSelecionados.length === 2
             if (!jaTem2Selecionados || estaSelecionado) {
                 personagemLI.classList.toggle('selecionado')
-    
+
                 if (!estaSelecionado) return this.adicionaSelecao(personagem)
-    
+
                 this.removeSelecao(personagem)
             }
-        }*/
+        }
 
         return personagemLI
     }
 
 
-    /*adicionaSelecao = (personagem) => {
+    adicionaSelecao = (personagem) => {
         this.personagensSelecionados.push(personagem)
         this.render()
     }
-    
-    
+
+
     removeSelecao = (personagem) => {
         const indexDoPersonagemNoArray = this.personagensSelecionados.indexOf(personagem)
         this.personagensSelecionados.splice(indexDoPersonagemNoArray, 1)
         this.render()
     }
-    
+
     escutarEventoDuelo() {
         const botaoDuelar = document.querySelector('.botao-duelar')
-    
+
         botaoDuelar.addEventListener('click', () => {
             if (this.personagensSelecionados.length < 2) return mostrarModal('Selecione 2 personagens')
-    
+
             const resultadoDuelo = Personagem.verificarVencedor(this.personagensSelecionados[0], this.personagensSelecionados[1])
-    
+
             mostrarModal(resultadoDuelo)
-    
+
             this.personagensSelecionados.splice(0, this.personagensSelecionados.length)
-    
+
             this.render()
         })
-    }*/
+    }
 }
